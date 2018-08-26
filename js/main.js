acd76970b8c4d9f7536633a0fc6515be4159a01d
alohaglenn <glenngonda@gmail.com>
2018-08-25 22:05:23 -0700

    filter out unqualified players by min games played

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 d3.json('data/stats.json').then(res => {
-  const dataSet = res.resultSets[0];
+  const headers = res.resultSets[0].headers;
+  const dataSet = res.resultSets[0].rowSet;
+
+  // Player must play 70% of his team's games (58 in 82-game season)
+  const qualifiedPlayers = dataSet.filter(player => {
+    return player[4] >= 58;
+  });
+
 });
