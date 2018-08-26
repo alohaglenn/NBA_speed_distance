e4b5bbe84b467bd6769775f9d44f96bf2114f4f9
alohaglenn <glenngonda@gmail.com>
2018-08-25 21:22:20 -0700

    init project

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app     = express();
+app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/js'));
+
+app.get('/',function(req,res){
+  res.sendFile('index.html');
+});
+
+app.listen(3000);
+
+console.log("Running at Port 3000");
